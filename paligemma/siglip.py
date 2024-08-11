06bc16988d6087dcfb13480cf82046999283ee9f
RomainSabathe <RSabathe@gmail.com>
2024-08-11 19:05:46 +0200

    Adding LayerNorm

--- paligemma/siglip.py
+++ paligemma/siglip.py
@@ -9,8 +9,9 @@ import torch.nn as nn
 class ViTConfiguration:
     image_size: int = 224
     in_channels: int = 3
-    hidden_size: int = 1_024  # "latent vector size D" in the paper
+    hidden_dim: int = 1_024  # "latent vector size D" in the paper
     patch_size: int = 16
+    num_layers: int = 3
 
     @property
     def num_patches(self) -> int:
@@ -61,12 +62,112 @@ class ImageTokenizer(nn.Module):
 def get_initial_positional_embeddings(
     num_patches: int, embedding_size: int
 ) -> torch.Tensor:
+    # TODO last 15 minutes: check which initialisation is chosen
     return nn.Parameter(torch.randn(num_patches, embedding_size))
 
 
+@dataclass
+class TransformerEncoderConfiguration:
+    sequence_length: int
+    num_layers: int = 3
+    hidden_dim: int = 728
+
+
 class TransformerEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, configuration: TransformerEncoderConfiguration):
         super(TransformerEncoder, self).__init__()
+        self.configuration = configuration
+        block_configuration = TransformerBlockConfiguration(
+            sequence_length=configuration.sequence_length,
+            hidden_dim=configuration.hidden_dim,
+        )
+        self.blocks = [
+            TransformerBlock(block_configuration)
+            for _ in range(configuration.num_layers)
+        ]
+        self.out_layer_norm = LayerNorm(
+            sequence_length=configuration.sequence_length,
+            hidden_dim=configuration.hidden_dim,
+        )
+
+    def forward(self, in_tokens_bnd: torch.Tensor) -> torch.Tensor:
+        x = in_tokens_bnd
+        for block in self.blocks:
+            x = block(x)
+
+        return self.out_layer_norm(x)
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, sequence_length: int, hidden_dim: int, eps: float = 1e-5):
+        super(LayerNorm, self).__init__()
+
+        self.eps = eps
+        self.bias_nd = nn.Parameter(torch.zeros(sequence_length, hidden_dim))
+        self.gain_nd = nn.Parameter(torch.ones(sequence_length, hidden_dim))
+
+    def forward(self, x_bnd: torch.Tensor) -> torch.Tensor:
+        mu_b11 = einops.reduce(x_bnd, "b n d -> b () ()", "mean")
+        variance_b11 = einops.reduce(
+            (x_bnd - mu_b11) ** 2.0, "b n d -> b () ()", "mean"
+        )
+        sigma_b11 = torch.sqrt(variance_b11 + self.eps)
+
+        x_bnd = (x_bnd / mu_b11) / sigma_b11
+        x_bnd = (x_bnd + self.bias_nd) * self.gain_nd
+        return x_bnd
+
+
+@dataclass
+class TransformerBlockConfiguration:
+    sequence_length: int
+    hidden_dim: int
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+
+    def forward(self, x_bnd: torch.Tensor) -> torch.Tensor:
+        pass
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+
+    def forward(self, x_bnd: torch.Tensor) -> torch.Tensor:
+        pass
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, configuration: TransformerBlockConfiguration):
+        super(TransformerBlock, self).__init__()
+        self.configuration = configuration
+
+        self.layer_norm_1 = LayerNorm(
+            sequence_length=configuration.sequence_length,
+            hidden_dim=configuration.hidden_dim,
+        )
+        self.layer_norm_2 = LayerNorm(
+            sequence_length=configuration.sequence_length,
+            hidden_dim=configuration.hidden_dim,
+        )
+        self.multihead_attention = MultiHeadAttention()
+        self.mlp = MLP()
+
+    def forward(self, x_bnd: torch.Tensor) -> torch.Tensor:
+        x_skip_bnd = x_bnd
+        x_bnd = self.layer_norm_1(x_bnd)
+        x_bnd = self.multihead_attention(x_bnd)
+        x_bnd += x_skip_bnd
+
+        x_skip_bnd = x_bnd
+        x_bnd = self.layer_norm_2(x_bnd)
+        x_bnd = self.mlp(x_bnd)
+        x_bnd += x_skip_bnd
+
+        return x_bnd
 
 
 class ClassificationHead(nn.Module):
@@ -79,34 +180,37 @@ class ViT(nn.Module):
         super(ViT, self).__init__()
         self.configuration = configuration
 
-        patch_embedder_configuration = ImageTokenizerConfiguration(
+        image_tokenizer_configuration = ImageTokenizerConfiguration(
             in_channels=configuration.in_channels,
-            embedding_size=configuration.hidden_size,
+            embedding_size=configuration.hidden_dim,
             patch_size=configuration.patch_size,
         )
-        self.patch_embedder = ImageTokenizer(patch_embedder_configuration)
+        self.image_tokenizer = ImageTokenizer(image_tokenizer_configuration)
         # Q: is this really the right way of writing/initialising the positional embedding?
         self.positional_embeddings = get_initial_positional_embeddings(
             num_patches=configuration.num_patches,
-            embedding_size=configuration.hidden_size,
+            embedding_size=configuration.hidden_dim,
         )
-        self.encoder = TransformerEncoder()
+        encoder_configuration = TransformerEncoderConfiguration(
+            sequence_length=configuration.num_patches,
+            num_layers=configuration.num_layers,
+            hidden_dim=configuration.hidden_dim,
+        )
+        self.encoder = TransformerEncoder(encoder_configuration)
         self.classification_head = ClassificationHead()
 
     def forward(
         self, imgs_pixels_bchw: torch.Tensor
     ) -> torch.Tensor:  # img_pixels or imgs_pixels ?
-        patches_nchw = break_images_into_patches(
-            imgs_pixels_bchw, patch_size=self.configuration.patch_size
-        )
-        patches_embeddings_nd = self.patch_embedder(patches_nchw)
-        patches_embeddings_nd += self.positional_embeddings
+        imgs_tokens_nd = self.image_tokenizer(imgs_pixels_bchw)
+        imgs_tokens_nd += self.positional_embeddings
 
         # TODO: add the special classification token
 
-        input_tokens_nd = patches_embeddings_nd
-        output_tokens_nd = self.encoder(input_tokens_nd)
+        output_tokens_nd = self.encoder(imgs_tokens_nd)
 
         # Q: what is this module supposed to return? Just the transformation of the classification token?
         #    the list of output embeddings?
-        return output_tokens_nd
+        return output_tokens_nd[
+            :, 0
+        ]  # returning the classification token for all items in the batch.
--- tests/test_siglip.py
+++ tests/test_siglip.py
@@ -5,6 +5,7 @@ from paligemma.siglip import (
     ViTConfiguration,
     ImageTokenizer,
     ImageTokenizerConfiguration,
+    LayerNorm,
 )
 
 batch_size = 2
@@ -22,6 +23,9 @@ def test_inference_vit():
     imgs = torch.randn(batch_size, channels, height, width, dtype=torch.float32)
     y = model(imgs)
 
+    assert isinstance(y, torch.Tensor)
+    assert y.dtype == torch.float32
+
 
 def test_inference_image_tokenizer():
     embedding_size = 3
@@ -37,3 +41,12 @@ def test_inference_image_tokenizer():
     assert imgs_tokens.shape[0] == batch_size
     assert imgs_tokens.shape[1] == num_patches
     assert imgs_tokens.shape[2] == embedding_size
+
+
+def test_inference_layer_norm():
+    layer_norm = LayerNorm(sequence_length=5, hidden_dim=10)
+    x_bnd = torch.randn(batch_size, 5, 10)
+    y_bnd = layer_norm(x_bnd)
+
+    assert y_bnd.shape == x_bnd.shape
+    assert y_bnd.dtype == y_bnd.dtype
